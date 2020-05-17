9d869e9c2b27b34ca4a8a62f847282134e604ed7
Oleksandr Kitaika <kitayka96@gmail.com>
2020-05-17 11:51:04 +0300

    hw10pro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,56 @@
 "use strict";
 
-let array = [1, [1.1, ['1.1.1'], 1.2], 2, [2.1, ['2.1.1', '2.1.2'], 2.2, 2.3], 3, 4];
-let generateList = function generateList(array) {
-    let ul = document.createElement('ul');
-    array.forEach(item => {
-        let li = document.createElement('li'); 
-        let liChild = null;
-        if (Array.isArray(item)) {
-            liChild = generateList(item);
-            li.style.listStyleType = "none";
-        }
-        else {
-            liChild = document.createTextNode(item);
-        }
-        li.append(liChild);
-        ul.append(li);
+const next = document.querySelector('.right-btn');
+const prev = document.querySelector('.left-btn');
+const items = document.querySelectorAll('.slider-item');
+const scrollBtns = document.querySelectorAll('.scroll_btn');
+
+
+function playSlider(items, prev, next) {
+    let numberOfSlide = 0;
+    items[numberOfSlide].classList.add('slider-play');
+
+    next.addEventListener('click', function (element) {
+        element.preventDefault();
+        removeStyle(); 
+        numberOfSlide++;
+        if (numberOfSlide == items.length) { numberOfSlide = 0; }
+        slideShow(); 		
     });
-    return ul;
+
+    prev.addEventListener('click', function (element) {
+        element.preventDefault();
+        removeStyle();
+        numberOfSlide--;
+        if (numberOfSlide == -1) { numberOfSlide = items.length - 1; }
+        slideShow();
+    });
+
+    scrollBtns.forEach(function (element) {  
+        element.addEventListener('click', function (elem) {
+            elem.preventDefault();
+            removeStyle();
+            numberOfSlide = this.textContent - 1;
+            slideShow();
+        });
+    });
+
+    
+    function removeStyle() {
+        items.forEach(function (item) {
+            item.classList.remove('slider-play');
+        });
+
+        scrollBtns.forEach(function (scrollBtns) {
+            scrollBtns.classList.remove('scroll__active');
+        });
+    }
+
+
+    function slideShow() {
+        items[numberOfSlide].classList.add('slider-play');
+        scrollBtns[numberOfSlide].classList.add('scroll__active');
+    }
 }
-document.body.append(generateList(array));
+
+playSlider(items, prev, next);
